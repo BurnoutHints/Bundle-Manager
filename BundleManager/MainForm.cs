c5fd47c52d44b72af4ffdf5563c3bb29009d2dad
Nathan V. <jebcraftserver@gmail.com>
2026-05-02 20:50:13 -0400

    fix build issues

--- BundleManager/MainForm.cs
+++ BundleManager/MainForm.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections;
 using System.ComponentModel;
+using System.IO;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
